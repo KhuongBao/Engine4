5c37c1cce12908d7568175ba32fc7b5b8d36b455
TheSuspecct <baogiakhuong2005@gmail.com>
2023-12-05 12:44:50 -0500

    Update engine4.ino

--- engine4.ino
+++ engine4.ino
@@ -1,66 +1,78 @@
-
 // Declaring variables
 
 // Sonar pins
-int echoPin = 8;
-int trigPin = 7;
+int echoPin = 8; //Echo to pin 8
+int trigPin = 7; //Trig to pin 7
 // L298 pins
-int in1 = 4; 
-int in2 = 5; 
-int in3 = 9; 
-int in4 = 10; 
+int in1 = 4; // IN1 to pin 4
+int in2 = 5; // IN2 to pin 5
+int in3 = 9; // IN3 to pin 9
+int in4 = 10; // IN4 to pin 10
 
-long duration;
-int distance;
 
 // Lights
-int headlight = 2;
-int breaklight = 3;
-int siren_blue = 11;
-int siren_red = 12;
-int night_light = 13;
+int breaklight = 6; //LED breaklights to 6
+int button_in = 2; // Button input to 2
+int siren_L = 11; // Left siren lights to 11
+int siren_R = 3; // Right siren lights to 3
 
+// Other variables
+long duration; // Blinking lights duration variable
+int distance; // Distance variable for ultrasonic sensor 
+int lightvolts = 168; // Voltage for lights ~3.3V
 
 void setup() {
 
   // Sonar digital pins
-  pinMode(trigPin, OUTPUT) ;
-  pinMode(echoPin, INPUT);
+  pinMode(trigPin, OUTPUT); // Set trig pin to output
+  pinMode(echoPin, INPUT); // Set echo pin to input
 
   // L298 pins output
-  pinMode(in1, OUTPUT); 
-  pinMode(in2, OUTPUT); 
-  pinMode(in3, OUTPUT); 
-  pinMode(in4, OUTPUT); 
-
-  Serial.begin (9600) ; 
+  pinMode(in1, OUTPUT); // Set IN1 pin to output
+  pinMode(in2, OUTPUT);  // Set IN2 pin to output
+  pinMode(in3, OUTPUT); // Set IN3 pin to output
+  pinMode(in4, OUTPUT); // Set IN4 pin to output
 
   // Lights
-  pinMode(headlight, OUTPUT);
-  pinMode(breaklight, OUTPUT);
-  pinMode(headlight, OUTPUT);
-  pinMode(breaklight, OUTPUT);
+  pinMode(breaklight, OUTPUT); // Set breaklight pin to output
+  pinMode(siren_L, OUTPUT); // Set left siren pin to output
+  pinMode(siren_R, OUTPUT); // Set right siren pin to output
+
+  // Button
+  pinMode(button_in, INPUT_PULLUP); // Set button input pin to INPUT with built-in resistor 
 
   // Starting light pins on low
-  digitalWrite(headlight, LOW);
-  digitalWrite(breaklight, LOW);
-  digitalWrite(siren_blue, LOW);
-  digitalWrite(siren_red, LOW);
+  analogWrite(breaklight, LOW); // Start breaklight pin on LOW (off)
+  analogWrite(siren_L, LOW); // Start left siren lights pin on LOW (off)
+  analogWrite(siren_R, LOW); // Start right siren lights pin on LOW (off)
   
 }
 
-// Blinking siren lights
+// Blinking siren lights function
 void sirenlight() {
-  digitalWrite(siren_blue, HIGH);
-  digitalWrite(siren_red, LOW);
-  delayMicroseconds(100);
-  digitalWrite(siren_blue, LOW);
-  digitalWrite(siren_red, HIGH);
-  delayMicroseconds(100);
+  int i=0; // Set interger iteration = 0
+  while (i<=10){ // Loop the function for 10 iterations
+
+      analogWrite(siren_L, LOW); // Turn left siren lights off
+      analogWrite(siren_R, lightvolts); // Turn right siren lights on (~3.3V)
+      delay(200); // Wait 0.2s
+      analogWrite(siren_L, lightvolts); // Turn left siren lights on (~3.3V)
+      analogWrite(siren_R, LOW); // Turn right siren lights off
+      delay(200); // Wait 0.2s
+      i++; // Increase interger iteration by 1
+    }
+
+    // After completing loop
+    analogWrite(siren_L, LOW); // Turn left siren lights off
+    analogWrite(siren_R, LOW); // Turn right siren lights off
 }
 
 
 void loop() {
+  // Siren light function
+  if (digitalRead(button_in) == LOW){ // If button is pressed
+    sirenlight(); // Call the siren light blinking function
+  }
   
   // Receiving information from ultrasonic sensors, calculating the distance 
   digitalWrite (trigPin, LOW);//starts trig pin low
@@ -72,56 +84,43 @@ void loop() {
 
   duration = pulseIn (echoPin, HIGH); //reads echo as it receives the pulse 
   distance = duration * 0.034 / 2; // Converts the time of flight to distance to object
-
-  // Displays the distance on the serial Monitor
-  Serial.print ("Distance: ");
-  Serial.print (distance);
-  Serial.println(" cm");
   
 
-// When distance is less than or equal to 15cm, move backwards, turn on both lights and siren lights
+// When distance is less than or equal to 15cm, move backwards
   if (distance <= 15) {
-    digitalWrite(in1, LOW);
-    digitalWrite(in2, HIGH);
-    digitalWrite(in3, LOW);
-    digitalWrite(in4, HIGH);
-    digitalWrite(headlight, HIGH);
-    digitalWrite(breaklight, HIGH);
-    sirenlight();
+    analogWrite(breaklight, LOW); // Set break lights to LOW (off)
+    digitalWrite(in1, LOW); // Turn left motor anticlock wise (Backwards)
+    digitalWrite(in2, HIGH); // Turn left motor anticlock wise (Backwards)
+    digitalWrite(in3, LOW); // Turn right motor anticlock wise (Backwards)
+    digitalWrite(in4, HIGH); // Turn right motor anticlock wise (Backwards)
 
 // When distance is less than or equal to 30cm, stop moving, turn on breaklights only and siren lights
   } else if (distance <= 30) {
 
-    digitalWrite(in1, LOW);
-    digitalWrite(in2, LOW);
-    digitalWrite(in3, LOW);
-    digitalWrite(in4, LOW);
-    digitalWrite(headlight, LOW);
-    digitalWrite(breaklight, HIGH);
-    sirenlight();
+    digitalWrite(in1, LOW); // Stop left motor
+    digitalWrite(in2, LOW); // Stop left motor
+    digitalWrite(in3, LOW); // Stop right motor
+    digitalWrite(in4, LOW); // Stop right motor
+    analogWrite(breaklight, lightvolts); // Turn break lights on (~3.3V)
 
-// When distance is less than or equal to 45cm, move forwards, turn on headlights only and siren lights
+// When distance is less than or equal to 45cm, move forwards
   } else if (distance <= 45) {
-
-    digitalWrite(in1, HIGH);
-    digitalWrite(in2, LOW);
-    digitalWrite(in3, HIGH);
-    digitalWrite(in4, LOW);
-    digitalWrite(headlight, HIGH);
-    digitalWrite(breaklight, LOW);
-    sirenlight();
+    analogWrite(breaklight, LOW); // Set break lights to LOW (off)
+    digitalWrite(in1, HIGH); // Turn left motor clock wise (Forwards)
+    digitalWrite(in2, LOW); // Turn left motor clock wise (Forwards)
+    digitalWrite(in3, HIGH); // Turn right motor clock wise (Forwards)
+    digitalWrite(in4, LOW); // Turn right motor clock wise (Forwards)
 
 
-// When distance is larger than 45cm, stop moving (out of range), torn on breaklighs only
+// When distance is larger than 45cm, stop moving (out of range), turn on breaklighs only
   } else {
 
-    digitalWrite(in1, LOW);
-    digitalWrite(in2, LOW);
-    digitalWrite(in3, LOW);
-    digitalWrite(in4, LOW);
-    digitalWrite(headlight, LOW);
-    digitalWrite(breaklight, HIGH);
+    digitalWrite(in1, LOW); // Stop left motor
+    digitalWrite(in2, LOW); // Stop left motor
+    digitalWrite(in3, LOW); // Stop right motor
+    digitalWrite(in4, LOW); // Stop right motor
+    analogWrite(breaklight, lightvolts); // Turn break lights on (~3.3V)
 
   }
-  delay(10);
+  delay(50); // Delay 0.05s
 }
